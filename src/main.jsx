3609d90545dd2e9e3dea4bc8a7b5123c3c28d500
dgenereal <158507822+dgenereal@users.noreply.github.com>
2026-07-02 14:34:40 +0100

    Initial project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,5 +7,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-const firstproject = ["epstein", "julia"]
