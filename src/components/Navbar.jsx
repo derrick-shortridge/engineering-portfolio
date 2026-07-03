947657c1dd20842cd9b11ac66e5379401b31176f
dgenereal <158507822+dgenereal@users.noreply.github.com>
2026-07-03 10:23:33 +0100

    Navbar hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@
 function Navbar() {
     return(
         <nav className ="flex justify-between items-center p-4 ">
-            <div classsName =" ">Derrick</div>
-            <div className="flex gap-6">
-                <a>Home</a>
-                <a>About</a>
-                <a>Projects</a>
-                <a>Contact</a>
+            <div className="font-bold text-xl">Derrick</div>
+            <div className="flex gap-6 text-sm">
+                <a href="#" className="text-black hover:text-blue-400">Home</a>
+                <a href="#" className="text-black hover:text-blue-400">About</a>
+                <a href="#" className="text-black hover:text-blue-400">Projects</a>
+                <a href="#" className="text-black hover:text-blue-400">Contact</a>
             </div>
         </nav>
     )
