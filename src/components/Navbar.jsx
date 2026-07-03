4130cedea0f2601015eaa2cf5a91f6c8f2ed912d
dgenereal <158507822+dgenereal@users.noreply.github.com>
2026-07-03 10:29:51 +0100

    navbar colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@
 
 function Navbar() {
     return(
-        <nav className ="flex justify-between items-center p-4 ">
-            <div className="font-bold text-xl">Derrick</div>
+        <nav className =" bg-black flex justify-between items-center p-4 sticky top-0 backdrop-blur-2xl">
+            <div className="font-bold text-xl text-white">Derrick</div>
             <div className="flex gap-6 text-sm">
-                <a href="#" className="text-black hover:text-blue-400">Home</a>
-                <a href="#" className="text-black hover:text-blue-400">About</a>
-                <a href="#" className="text-black hover:text-blue-400">Projects</a>
-                <a href="#" className="text-black hover:text-blue-400">Contact</a>
+                <a href="#" className="text-white hover:text-blue-400">Home</a>
+                <a href="#" className="text-white hover:text-blue-400">About</a>
+                <a href="#" className="text-white hover:text-blue-400">Projects</a>
+                <a href="#" className="text-white hover:text-blue-400">Contact</a>
             </div>
         </nav>
     )
